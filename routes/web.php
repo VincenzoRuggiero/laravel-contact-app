1b65d622c11432abd8ad318755ac322abceb18e2
Vincenzo Ruggiero <45271214+VincenzoRuggiero@users.noreply.github.com>
2023-06-17 12:45:43 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contacts', function () {
+    return "<h1>All contacts</h1>";
+});
+
+
+Route::get('/contacts/create', function () {
+    return "<h1>Add new Contact</h1>";
+});
+
+// 
+Route::get('/contacts/{id}', function ($id) {
+    return "Contact " . $id;
+})->whereNumber('id'); // ->where('id', '[0-9]+'); // Only numbers accepted
+
+
+Route::get('/companies/{name?}', function ($name = null) {
+    if ($name) {
+        return "Company " . $name;
+    } else {
+        return "All companies";
+    }
+})->whereAlphaNumeric('name'); // Acceptes numbers and letters
+
+// ->whereAlpha('name');  
+//->where('name', '[a-zA-Z]+'); // Only letters accepted
